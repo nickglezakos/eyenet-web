9c069e1a65aa593be33510a82be3b30aeb2f2174
nick.glezakos <nick.glezakos@gmail.com>
2026-07-17 12:08:28 +0300

    Wire 'Manage From Anywhere' CTA to Matrix deep-dive card
    - Added id='dd-card-matrix' to the Eyenet Matrix deep-dive card wrapper
    - Changed Card 7 CTA href from #value-across-every-role to #dd-card-matrix
    - Tapping 'Manage From Anywhere' now scrolls to Matrix deep-dive section

--- _push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /Users/nickgklezakos/Documents/eyenet-web
-git add -A
-git commit -m "Final push: all UX refinements (header, responsive cards, Matrix, CTA wiring)"
-git push origin main
