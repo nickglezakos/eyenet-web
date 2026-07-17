ff3382734c60b2ea28394522584d1c41eab4c430
nick.glezakos <nick.glezakos@gmail.com>
2026-07-17 11:59:16 +0300

    Final push: all UX refinements (header, responsive cards, Matrix, CTA wiring)

---
+++ _push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/nickgklezakos/Documents/eyenet-web
+git add -A
+git commit -m "Final push: all UX refinements (header, responsive cards, Matrix, CTA wiring)"
+git push origin main
